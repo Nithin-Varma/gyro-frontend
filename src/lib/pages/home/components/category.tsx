9d0453fd16a3719be8392b97a8a6cd15244560c5
Nithin-Varma <mnithin1422@gmail.com>
2023-03-29 20:43:39 +0530

    driver/rider

--- src/lib/pages/home/components/category.tsx
+++ src/lib/pages/home/components/category.tsx
@@ -4,21 +4,26 @@ const ChooseOne = () => (
   <SimpleGrid
     columns={{ base: 1, md: 2 }}
     spacing={{ base: 14, md: 10 }}
-    p={{ base: "", md: "28" }}
+    p={{ base: "8", md: "48" }}
   >
     <Box
-      border="2px"
+      border="1px"
       paddingX="12"
       paddingY={12}
+      bgColor="#6C0BA9"
+      color="white"
+      borderRadius="3xl"
       _hover={{
         transform: "scale(0.98)",
         borderColor: "#6C0BA9",
+        bgColor: "white",
+        color: "#6C0BA9",
         // filter: "blur(20px)",
         // shadow: "2px 4px 5px #6C0BA9, 2px 4px 5px #6C0BA9",
         boxShadow: "0px 1px 10px 2px #6C0BA9, 0 4px 4px 2px #6C0BA9",
       }}
     >
-      <Link>
+      <Link _hover={{ textDecoration: "none" }}>
         <Heading textAlign="center">Signup as a Driver</Heading>
       </Link>
     </Box>
@@ -27,18 +32,20 @@ const ChooseOne = () => (
       paddingX="12"
       paddingY={12}
       bgColor="#6C0BA9"
+      color="white"
+      borderRadius="3xl"
       _hover={{
         transform: "scale(0.98)",
         borderColor: "#6C0BA9",
+        bgColor: "white",
+        color: "#6C0BA9",
         // filter: "blur(20px)",
         // shadow: "2px 4px 5px #6C0BA9, 2px 4px 5px #6C0BA9",
         boxShadow: "0px 1px 10px 2px #6C0BA9, 0 4px 4px 2px #6C0BA9",
       }}
     >
-      <Link _hover={{ textDecoration: "none"}}>
-        <Heading textAlign="center" color="white" _hover={{}}>
-          Signup as a Rider
-        </Heading>
+      <Link _hover={{ textDecoration: "none" }}>
+        <Heading textAlign="center">Signup as a Rider</Heading>
       </Link>
     </Box>
   </SimpleGrid>
