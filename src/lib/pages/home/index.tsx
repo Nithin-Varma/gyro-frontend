649f427851a70b68a9af3883ba7d213feb653ba1
Nithin-Varma <mnithin1422@gmail.com>
2023-03-29 11:19:41 +0530

    faq

--- src/lib/pages/home/index.tsx
+++ src/lib/pages/home/index.tsx
@@ -2,13 +2,14 @@ import { Grid, Heading } from "@chakra-ui/react";
 
 import Header from "../../layout/Header";
 
+import FAQ from "./components/faq";
 import LandingView from "./components/Landing_view.tsx";
 // import Header from "lib/layout/Header";
 
 // /home/nithin/gyro-frontend/src/lib/pages/home/components/Landing_view.tsx
 // import CTASection from "./components/Landing_view";
-import WhyToUse from "./components/WhyToUse";
 import OnChain100 from "./components/SomeTextandImage";
+import WhyToUse from "./components/WhyToUse";
 
 const Home = () => {
   return (
@@ -17,6 +18,7 @@ const Home = () => {
       <LandingView />
       <WhyToUse />
       <OnChain100 />
+      <FAQ />
     </Grid>
   );
 };
