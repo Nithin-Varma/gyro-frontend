e26d8c0d4b2939d98387474b5db05002205b0566
Nithin-Varma <mnithin1422@gmail.com>
2023-03-31 21:18:39 +0530

    routing

--- src/lib/router/routes.tsx
+++ src/lib/router/routes.tsx
@@ -1,11 +1,16 @@
 import type { PathRouteProps } from "react-router-dom";
 
 import Home from "lib/pages/home";
+import DriverSignup from "lib/pages/Signup_Login/Driver";
 
 export const routes: Array<PathRouteProps> = [
   {
     path: "/",
     element: <Home />,
+  },
+  {
+    path: "/driversignup",
+    element: <DriverSignup />,
   },
 ];
 
